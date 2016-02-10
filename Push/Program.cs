9755558caa6a696b66ba6cedd6f8a1c64313eca9
Kevin Bowe <kevinbowe1957@gmail.com>
2016-02-10 13:37:20 -0500

    ISSUE - Prevent more than one instance of Push from running.
    MOD - Rewrite Main() to use Mutex. Code tests to see if the named
    instance "PushApp" is running and either starts the instance or tells
    the user that an instance of Push App is running.

--- Push/Program.cs
+++ Push/Program.cs
@@ -8,39 +8,60 @@ namespace Push
 		[STAThread]
 		static void Main()
 		{
-			// This loads the properties, defined in AppSettings, with values...
-			//		If the value of a property has not been defined, all properties will be set to null...
-			appSettings.Load();
 
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
+			bool mutexCreated = true;
+			using (System.Threading.Mutex mutex = new System.Threading.Mutex(true, "PushApp", out mutexCreated))
+			{
+				if (mutexCreated)
+				{
+					// This loads the properties, defined in AppSettings, with values...
+					//		If the value of a property has not been defined, all properties will be set to null...
+					appSettings.Load();
 
-			MainForm mainForm = new MainForm(appSettings);
-			
-			// Assign the FormClosed Event handler...
-			mainForm.FormClosed += FormClosed;
+					Application.EnableVisualStyles();
+					Application.SetCompatibleTextRenderingDefault(false);
 
-			// This handles the first-run + cancel scenario...
-			if (mainForm.IsDisposed)
-				return;
+					MainForm mainForm = new MainForm(appSettings);
 
-			Application.Run(mainForm);
+					// Assign the FormClosed Event handler...
+					mainForm.FormClosed += FormClosed;
 
-			#region [ Comments ]
-			/* When the form is closed, the Form.OnClosing and Form.OnFormClosed 
-			 *		events is executed.
-			 * 
-			 *	The Form.OnClosing event calls Itenso.Configuration.FormSettings.FormClosing( ). 
-			 *	This method takes care of writing the form properties to the XML File.
-			 * 
-			 *	Next the Form.OnFormClosed event calls Program.FormClosed( ).
-			 *	This method copies the PushSettings properties into the 
-			 *		AppSettings collection.   */
-			#endregion			
-			
-			// Save the AppSettings to the same XML file that holds the form properties...
-			appSettings.Save();
+					// This handles the first-run + cancel scenario...
+					if (mainForm.IsDisposed)
+						return;
 
+					Application.Run(mainForm);
+
+					#region [ Comments ]
+					/* When the form is closed, the Form.OnClosing and Form.OnFormClosed 
+					 *		events is executed.
+					 * 
+					 *	The Form.OnClosing event calls Itenso.Configuration.FormSettings.FormClosing( ). 
+					 *	This method takes care of writing the form properties to the XML File.
+					 * 
+					 *	Next the Form.OnFormClosed event calls Program.FormClosed( ).
+					 *	This method copies the PushSettings properties into the 
+					 *		AppSettings collection.   */
+					#endregion
+
+					// Save the AppSettings to the same XML file that holds the form properties...
+					appSettings.Save();
+				}
+				else
+				{
+					// If we get here Push is already running. Don't let another copy start...
+					System.Diagnostics.Process current = System.Diagnostics.Process.GetCurrentProcess();
+					foreach (System.Diagnostics.Process process in System.Diagnostics.Process.GetProcessesByName(current.ProcessName))
+					{
+						if (process.Id != current.Id)
+						{
+							MessageBox.Show("Another instance of Push is already running.", "Push already running", 
+									MessageBoxButtons.OK, MessageBoxIcon.Information);
+							break;
+						}
+					}
+				} //END_MUTEX-CREATED
+			} // END_USING_MUTEX
 		} // END_METHOD
 
 
